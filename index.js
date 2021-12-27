ae9c7509f66017c4577bef73db25dab9c18d909f
Tanvirul islam <tanvir.topader345@gmail.com>
2021-12-27 01:42:32 -0600

    restfull api

--- index.js
+++ index.js
@@ -20,6 +20,8 @@ async function run() {
       await client.connect();
       const database = client.db("Block-chain-database");
       const popularItems = database.collection("popular-items");
+      const popularItemscomments = database.collection("popular-items-comments");
+      const featuredItemscomments = database.collection("featured-items-comments");
       const featuredItems = database.collection("featured-items");
 
       //find all popularitems
@@ -28,12 +30,59 @@ async function run() {
         const result = await cursor.toArray();
         res.json(result);
     });
+
+      //find single popularitems
+      app.get('/popularitems/:id', async (req, res) => {
+        const id = req.params.id;
+        const query = { _id: ObjectId(id) };
+        const result = await popularItems.findOne(query);
+        res.json(result);
+    });
+
+    //Post all comments for popolar items
+    app.post('/popularitems/comments', async (req, res) => {
+      const product = req.body;
+      const result = await popularItemscomments.insertOne(product);
+      // console.log(result);
+      res.json(result);
+  });
+
+  //get all comments for popolar items
+    app.get('/popularitems/comments/all', async (req, res) => {
+      const cursor = popularItemscomments.find({});
+      const result = await cursor.toArray();
+      res.json(result);
+  });
+
+  
     
       //find all featureditems
       app.get('/featureditems', async (req, res) => {
         const cursor = featuredItems.find({});
         const result = await cursor.toArray();
-        console.log('find all data');
+        // console.log('find all data');
+        res.json(result);
+    });
+    //find single popularitems
+    app.get('/featureditems/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await featuredItems.findOne(query);
+      res.json(result);
+  });
+
+      //Post all comments for featured items
+      app.post('/featureditems/comments', async (req, res) => {
+        const product = req.body;
+        const result = await featuredItemscomments.insertOne(product);
+        // console.log(result);
+        res.json(result);
+    });
+
+      //get all comments for featured items
+      app.get('/featureditems/comments/all', async (req, res) => {
+        const cursor = featuredItemscomments.find({});
+        const result = await cursor.toArray();
         res.json(result);
     });
     
